6c9c45a633eb1edd0b3c53cd1ad0bd0d7faa990f
FireSmile01 <moi_igri00@mail.ru>
2017-03-16 15:19:46 +0700

    Add files via upload

--- АstarOOP/algoritm.js
+++ АstarOOP/algoritm.js
@@ -10,11 +10,11 @@ function start(){
 	start.getCoordinats(); // получаем i и j начала и конца
 	end.getCoordinats();
 	
-	start.g = 0; // g это стоимость точки, так как мы начинаем с этой точки, то логично, что шаг на нее равен 0
-	openList.push(start); // P.S на одну и туже точку нельзя вставать 2 раза, поэтому на старт мы не вернемся
+	start.g = 0; // g это расстояние от старта до текущей точки
+	openList.push(start); 
 	
-	while(end.div.style.borderColor != "red"){ // цикл будет работать пока финишь не будет найден, либо пока программа может
-			openList[0].div.style.borderColor = "blue";	 // проверять новые точки
+	while(end.div.style.borderColor != "red"){ // цикл будет работать пока финишь не будет найден, либо пока программа может проверять новые точки
+			openList[0].div.style.borderColor = "blue";	 // бордер клетки, на которую мы делаем шаг, красив в синий
 			addOpenList(openList[0], end);
 			if(openList.length == 0)
 				break;
@@ -23,7 +23,7 @@ function start(){
 		showWay(end);
 }
 
-function addOpenList(parentPoint, end){ 
+function addOpenList(parentPoint, end){ // функция отвечает за логику создания точек, и добавление точек в открытый и закрытый списки, цель функци - на шаг приблизиться к финишу
 	let i = parentPoint.y;
 	let j = parentPoint.x;
 	
@@ -37,32 +37,32 @@ function addOpenList(parentPoint, end){
 	for (let k = j-1; k < j+2; ++k)
 		for(let l = i-1; l < i+2; ++l)
 			if(l >= 0 && k >= 0 &&  k < divX && l < divY)
-				if( arr[k][l].role != "wall"  &&  arr[k][i].role != "wall"  && arr[j][l].role != "wall" &&
-				 arr[k][l].role != "step" && arr[k][l].role != "openList" && arr[k][l].role != "start"){
+				if( arr[k][l].role != "wall"  &&  arr[k][i].role != "wall"  && arr[j][l].role != "wall" && // услове на непроходимость стен
+				 arr[k][l].role != "step" && arr[k][l].role != "openList" && arr[k][l].role != "start"){ // и невозможность создать копию точки, которая была создана ранее
 					let point = new Point(arr[k][l], parentPoint);
 					point.getCoordinats();
 					point.role = "openList";
 				
-					if( point.div == arr[k][i] || point.div == arr[j][l])
-						point.g = point.parentPoint.g + 10;
+					if( point.div == arr[k][i] || point.div == arr[j][l]) // 	условие на то, что точки по диагонали от данной
+						point.g = point.parentPoint.g + 10; // будут стоить 14, остальные 10
 					else 
 						point.g = point.parentPoint.g + 14;					
 					point.f = 10 * Point.getLength(point, end)  + point.g;
 				
-					if ( arr[k][l].role == "end"){
+					if ( arr[k][l].role == "end"){ // если очередная точка окажется финишом, то помечаем ее и выходим
 						end.div.style.borderColor = "red";
 						closeList.push(end);
 						end.parentPoint = parentPoint;
 						return;
 					}
-					openList.push(point);
+					openList.push(point); // найденная точка - очердная точка открыого списка
 				}
 	openList.sort(sortFunction);
-	for (let a in openList){
+	for (let a in openList){ // маркируем точки открытого списка
 		openList[a].div.style.background = "gray";
 		openList[a].div.role = "openList";
 	}
-	openList[0].role = "step";
+	openList[0].role = "step"; // в этом элементе открытого списка лежит точка, ближайшая к финишу, ее мы перенесем в закрытый список
 
 	for(let i in closeList)
 		if(closeList[i].x === openList[0].x && closeList[i].y === openList[0].y)
@@ -73,12 +73,12 @@ function sortFunction(a, b){
 	return Number(a.f) - Number(b.f); 
 }
 
-function showWay(elementOfWay){
-		let classProp = elementOfWay.div.firstChild.classList;
+function showWay(elementOfWay){ // у всех точек есть дивы, в которых есть дивы со свойством hidden, просто уберем это свойство с
+		let classProp = elementOfWay.div.firstChild.classList;  //  тех точек, по которым находится кротчайший путь
 		classProp.remove("hidden");
 		
 		if(elementOfWay.parentPoint)
-			showWay(elementOfWay.parentPoint);
+			showWay(elementOfWay.parentPoint); 
 		else
 			return;
 }
